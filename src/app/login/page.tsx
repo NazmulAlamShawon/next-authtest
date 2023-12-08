6b1f67d0ad0af3b34e98064021c901c1904c04f7
NazmulAlamShawon <shawonkst15@gmail.com>
2023-12-08 17:55:13 +0600

    add new

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default  function LoginPage (){
+    return (
+        <div className="flex justify-center items-center m-5 p-3">
+            <h1 className="text-2xl"> login</h1>
+        </div>
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignupPage (){
+    return (
+        <div className="flex justify-center items-center">
+            <h1>signp</h1>
+        </div>
+    )
+}
