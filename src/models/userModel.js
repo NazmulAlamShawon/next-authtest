8f0f897ae179a13cac9ab0329483ac8fe8d12343
NazmulAlamShawon <shawonkst15@gmail.com>
2023-12-08 19:59:23 +0600

    add new router

---
+++ src/models/userModel.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+const userSchema = new mongoose.Schema({ 
+    username : {
+         type: String,
+         required: [true, "please provide a valid username"],
+         unique: true,
+    },
+    email : {
+        type: String,
+        required: [true, "please provide a valid email"],
+        unique: true,
+   },
+
+ password : {
+    type: String,
+    required: [true, "please provide a valid password"],
+    
+},
+isVerified : {
+    type: Boolean,
+    default: false,
+},
+isAdmin :{
+    type: Boolean,
+    default: false,
+},
+forgotPaaswordToken: String,
+forgotPaaswordTokenExpiry: Date,
+vrifyToken: String,
+vrifyTokenExpiry: Date,
+
+});
+const User = mongoose.models.users || mongoose.model("users",userSchema);
+
+export default User;
